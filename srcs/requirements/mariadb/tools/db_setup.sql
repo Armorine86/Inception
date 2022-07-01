e5400feb8d43955cd5e0953c02cd1f249c3c1aaa
Armorine86 <mmondell@student.42quebec.com>
2022-07-01 16:37:04 -0400

    MariaDB seeeeems to work... Weird socket error still

---
+++ srcs/requirements/mariadb/tools/db_setup.sql
@@ -0,0 +1,14 @@
+-- Creates a database if one of the same name doesn't already exists
+CREATE DATABASE IF NOT EXISTS ${MARIA_WORDPRESS_DB};
+
+-- Creates a new user
+CREATE USER IF NOT EXISTS ${MARIA_DB_USER}@'localhost' IDENTIFIED BY '${MARIA_DB_USER_PWD}';
+
+-- Grant all privilege on the new database to the new user
+GRANT ALL PRIVILEGES ON ${MARIA_WORDPRESS_DB}.* TO ${MARIA_DB_USER} IDENTIFIED BY '${MARIA_DB_USER_PWD}';
+
+-- Resets database root password
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIA_DB_NEW_ROOT_PWD}';
+
+-- Force grant table to be updated
+FLUSH PRIVILEGES;
---
+++ srcs/requirements/mariadb/tools/start.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -exo pipefail
+
+SUCCESS=/var/lib/mysql/.setup_done
+
+echo $@
+
+if [ -f "$SUCCESS" ]
+then
+	echo "Database is already configured... Starting MariaDB"
+	/usr/bin/mysqld
+else
+	# Start mysql service
+	/etc/init.d/mysql start
+
+	# envsubst is used to substitute shell format strings with environment variables in text.
+	# redirect the config with expended variable into initdb.sql
+	envsubst < db_setup.sql > initdb.sql
+
+	echo "Starting mysql"
+	/usr/bin/mysql
+
+	echo "Attempting to setup mariadb with initdb.sql file as root"
+	mariadb -uroot -proot < initdb.sql
+
+	# If setup is successful, create a hidden file to mark setuping as completed
+	touch /var/lib/mysql/.setup_done
+
+	echo "Setup successful... Removin initdb.sql file"
+	rm initdb.sql
+	rm db_setup.sql
+
+	killall mysqld
+fi
+
+exec "$@"
